af321fc22ae0fa34451caa57e90218b35259dbbd
Felix Biego <45796740+fbiego@users.noreply.github.com>
2022-03-19 05:22:56 +0300

    Update check.php

--- check.php
+++ check.php
@@ -1,31 +1,40 @@
 <?php
-   if ($_GET['s']){
-       $q = $_GET['s'];
-   }
-   
-   function search($query){
-       $file = file("../../daraja.txt");
-       $file = array_reverse($file);
-       
-       $merchant = '"merchantID":"'.$query.'"';
-       $checkout = '"checkoutID":"'.$query.'"';
-       $transaction = '"txID":"'.$query.'"';
-       foreach($file as $line){
-           if (strpos($line, $merchant) !== false) {
-               return $line;
-           }
-           if (strpos($line, $checkout) !== false) {
-               return $line;
-           }
-           if (strpos($line, $transaction) !== false) {
-               return $line;
-           }
-           
-       }
-       return '{"resultCode":-1,"resultDesc":"Transaction not found"}';
-   }
-   
-   header('Content-type: application/json');
-   echo search($q);
-   
-   ?>
+function search($query)
+{
+    $file = file("daraja.txt");
+    $file = array_reverse($file);
+
+    $merchant = '"merchantID":"' . $query . '"';
+    $checkout = '"checkoutID":"' . $query . '"';
+    $transaction = '"txID":"' . $query . '"';
+    foreach ($file as $line)
+    {
+        if (strpos($line, $merchant) !== false)
+        {
+            return $line;
+        }
+        if (strpos($line, $checkout) !== false)
+        {
+            return $line;
+        }
+        if (strpos($line, $transaction) !== false)
+        {
+            return $line;
+        }
+
+    }
+    return '{"success":false,"message":"Transaction not found"}';
+}
+
+header('Content-type: application/json');
+if (isset($_GET['query']))
+{
+    $q = $_GET['query'];
+    echo search($q);
+}
+else
+{
+    echo '{"success":false,"message":"Specify a merchant or checkout code using \'?query=\'"}';
+}
+
+?>
