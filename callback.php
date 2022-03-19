661a68c9e70fefcc32f4977b9a95da5d75e666b3
Felix Biego <45796740+fbiego@users.noreply.github.com>
2022-03-19 05:26:06 +0300

    Update callback.php

--- callback.php
+++ callback.php
@@ -1,21 +1,33 @@
 <?php
-   $json = file_get_contents('php://input');
-   $data = json_decode($json, true);
-   
-   $result['merchantID'] = $data['Body']['stkCallback']['MerchantRequestID'];
-   $result['checkoutID'] = $data['Body']['stkCallback']['CheckoutRequestID'];
-   $result['resultCode'] = $data['Body']['stkCallback']['ResultCode'];
-   $result['resultDesc'] = $data['Body']['stkCallback']['ResultDesc'];
-   
-   if ($result['resultCode'] == 0){
-       //successful transaction
-       $result['amount'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][0]['Value'];
-       $result['txID'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value'];
-       $result['balance'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][2]['Value'];
-       $result['date'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][3]['Value'];
-       $result['number'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][4]['Value'];
-   }
-   
-   $encoded = json_encode($result);
-   file_put_contents("../../daraja.txt", $encoded."\n", FILE_APPEND);
-   ?>
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+$result['success'] = true;
+
+if ($result['resultCode'] == 0)
+{
+    $result['paid'] = true;
+    $result['message'] = "Payment successful";
+
+    //successful transaction
+    $result['amount'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][0]['Value'];
+    $result['txID'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value'];
+    $result['balance'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][2]['Value'];
+    $result['date'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][3]['Value'];
+    $result['number'] = $data['Body']['stkCallback']['CallbackMetadata']['Item'][4]['Value'];
+
+}
+else
+{
+    $result['paid'] = false;
+    $result['message'] = "Payment cancelled";
+}
+
+$result['merchantID'] = $data['Body']['stkCallback']['MerchantRequestID'];
+$result['checkoutID'] = $data['Body']['stkCallback']['CheckoutRequestID'];
+$result['resultCode'] = $data['Body']['stkCallback']['ResultCode'];
+$result['resultDesc'] = $data['Body']['stkCallback']['ResultDesc'];
+
+$encoded = json_encode($result);
+file_put_contents("daraja.txt", $encoded . "\n", FILE_APPEND);
+?>
